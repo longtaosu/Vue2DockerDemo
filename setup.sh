1675b106b91df05459cd65bdad68360b133922d7
imstrive <longtaosu@sina.com>
2019-05-15 09:42:24 +0800

    add nginx dockerfile etc

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ docker stop vue2docker;
 docker rm vue2docker;
 docker build -t vue2docker:$image_version .;
 docker images;
-docker run -p 10002:80 -d --name vue2docker vue2docker:$image_version;
+docker run -p 10003:80 -d --name vue2docker vue2docker:$image_version;
 # -v ~/docker-data/house-web/appsettings.json:/app/appsettings.json -v ~/docker-data/house-web/NLogFile/:/app/NLogFile   --restart=always
 docker logs vue2docker;
 #删除build过程中产生的镜像    #docker image prune -a -f
